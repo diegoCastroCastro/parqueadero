076d2710f4e454709af73e90a877e1c89138e1d1
diegoCastroCastro <diegomarshall4@gmail.com>
2020-11-17 22:39:31 -0500

    FacturaDAO-GestionFacturaON

--- src/main/java/ec/ups/edu/appdis/g2/parqueadero/dao/FacturaDAO.java
+++ src/main/java/ec/ups/edu/appdis/g2/parqueadero/dao/FacturaDAO.java
@@ -1,10 +1,36 @@
 package ec.ups.edu.appdis.g2.parqueadero.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 import ec.ups.edu.appdis.g2.parqueadero.modelo.Factura;
 
+@Stateless
 public class FacturaDAO {
 	
-	public boolean insert (Factura entify) {
+	@Inject
+	private Connection con;
+	
+	public boolean insert (Factura entity) throws SQLException {
+		
+		String sql = "INSERT INTO Factura (numero, fechaEmision, valorTotal, ticket, cliente )"
+				+ "VALUES(?, ?, ?, ?, ?)";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, entity.getNumero());
+		ps.setDate(2, (Date)entity.getFechaEmision());
+		ps.setDouble(3, entity.getValorTotal());
+		ps.setObject(4, entity.getTicket());
+		ps.setObject(5, entity.getCliente());
+		
+		ps.execute();
+		ps.close();
+		
 		return true;
 	}
 	public boolean update (Factura entify) {
--- src/main/java/ec/ups/edu/appdis/g2/parqueadero/negocio/GestionFacturaON.java
+++ src/main/java/ec/ups/edu/appdis/g2/parqueadero/negocio/GestionFacturaON.java
@@ -1,12 +1,29 @@
 package ec.ups.edu.appdis.g2.parqueadero.negocio;
 
+import java.sql.SQLException;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import ec.ups.edu.appdis.g2.parqueadero.dao.FacturaDAO;
 import ec.ups.edu.appdis.g2.parqueadero.modelo.Cliente;
 import ec.ups.edu.appdis.g2.parqueadero.modelo.Factura;
 import ec.ups.edu.appdis.g2.parqueadero.modelo.Ticket;
 
+@Stateless
 public class GestionFacturaON {
 	
+	@Inject
+	private FacturaDAO daoFactura;
+	
 	public boolean registrarFactura(Factura factura) {
+		try {
+			daoFactura.insert(factura);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return true;
 	}
 	
