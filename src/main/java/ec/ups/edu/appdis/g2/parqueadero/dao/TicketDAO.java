2d492adb4e6ce016a15680a9bfc40b1fffcd1ab0
diegoCastroCastro <diegomarshall4@gmail.com>
2020-11-18 08:59:07 -0500

    VehiculoDAO

--- src/main/java/ec/ups/edu/appdis/g2/parqueadero/dao/TicketDAO.java
+++ src/main/java/ec/ups/edu/appdis/g2/parqueadero/dao/TicketDAO.java
@@ -2,10 +2,10 @@ package ec.ups.edu.appdis.g2.parqueadero.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.util.Date;
+import java.sql.SQLException;
+import java.sql.Date;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-
 import ec.ups.edu.appdis.g2.parqueadero.modelo.Ticket;
 
 @Stateless
@@ -13,14 +13,14 @@ public class TicketDAO {
 	
 	@Inject
 	private Connection con;
-	public boolean insert (Ticket entity) {
+	public boolean insert (Ticket entity) throws SQLException {
 		String sql = "INSERT INTO Cliente (codigo, fechaIngreso, fechaSalida, tiempo, valor, vehiculo)"
 				+ " VALUES(?, ?, ?, ?, ?, ?)";
 		
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setInt(1, entity.getCodigo());
-		ps.setDate(2, (java.sql.Date) entity.getFechaIngreso());
-		ps.setDate(3, entity.getFechaSalida());
+		ps.setDate(2, (Date) entity.getFechaIngreso());
+		ps.setDate(3, (Date) entity.getFechaSalida());
 		ps.setInt(4, entity.getTiempo());
 		ps.setDouble(5, entity.getValor());
 		ps.setObject(4, entity.getVehiculo());
--- src/main/java/ec/ups/edu/appdis/g2/parqueadero/dao/VehiculoDAO.java
+++ src/main/java/ec/ups/edu/appdis/g2/parqueadero/dao/VehiculoDAO.java
@@ -1,10 +1,33 @@
 package ec.ups.edu.appdis.g2.parqueadero.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 import ec.ups.edu.appdis.g2.parqueadero.modelo.Vehiculo;
 
+@Stateless
 public class VehiculoDAO {
 	
-	public boolean insert (Vehiculo entify) {
+	@Inject
+	private Connection con;
+	
+	public boolean insert (Vehiculo entity) throws SQLException {
+		
+		String sql = "INSERT INTO Vehiculo (color, marca, placa)"
+				+ "VALUES (?, ?, ?)";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, entity.getColor());
+		ps.setInt(2, entity.getMarca());
+		ps.setInt(3, entity.getPlaca());
+		
+		ps.executeUpdate();
+		ps.close();
+		
 		return true;
 	}
 	public boolean update (Vehiculo entify) {
